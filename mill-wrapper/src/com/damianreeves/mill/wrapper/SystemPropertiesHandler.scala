ddabb4a6106f9d23ffc2bf91514c88e7c3667fb2
Michelle <michelchan421@Gmail.com>
2022-09-09 12:25:42 -0400

    Added syspprop handler and started test

---
+++ mill-wrapper/src/com/damianreeves/mill/wrapper/SystemPropertiesHandler.scala
@@ -0,0 +1,27 @@
+package io.eleven19.mill.wrapper
+
+import java.nio.file.{Files, Path}
+import java.util.Properties
+import java.io.IOException
+import scala.util.matching.Regex
+import scala.jdk.CollectionConverters.*
+
+object SystemPropertiesHandler {
+
+  final val SYSPROP_PATTERN = "systemProp\\.(.*)".r
+
+  def getSystemProperties(propertiesFile: Path): Map[String, String] =
+    if (!Files.isRegularFile(propertiesFile)) Map.empty
+
+    val properties = new Properties()
+    try
+      properties.load(Files.newInputStream(propertiesFile))
+    catch
+      case io: IOException => throw new RuntimeException( "Error when loading properties file=" + propertiesFile, io);
+
+    properties.entrySet.asScala.foldLeft(Map.empty[String, String]) { (map, entry) =>
+      SYSPROP_PATTERN.findFirstIn(entry.getKey.toString) match
+        case Some(value) if (!value.isEmpty) => map + (value -> entry.getKey.toString)
+        case _ => map
+    }
+}
---
+++ mill-wrapper/test/src/com/damianreeves/mill/wrapper/SystemPropertiesHandlerSpec.scala
@@ -0,0 +1,7 @@
+package io.eleven19.mill.wrapper
+
+import zio.test._
+
+object SystemPropertiesHandlerSpec extends ZIOSpecDefault{
+  def spec = suite("SystemPropertiesHandlerSpec")()
+}
